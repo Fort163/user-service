ab5bcf32a5cc65089f870232ac3aeeb7ab9bb21b
Fort163 <Front-91@mail.ru>
2023-11-02 12:29:34 +0400

    add settings

--- src/main/java/com/quick/recording/user/service/UserServiceApplication.java
+++ src/main/java/com/quick/recording/user/service/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.quick.recording"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.quick.recording.gateway.service")
 public class UserServiceApplication {
